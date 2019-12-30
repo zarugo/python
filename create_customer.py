d5f349f7d706c0760fce3e845f041ffaf3bd030c
Luigi Zarlenga <luigi.zarlenga@faacspa.local>
2019-12-30 12:09:07 +0100

    added all the possible data

--- create_customer.py
+++ create_customer.py
@@ -30,27 +30,36 @@ try:
     else:
         token = (json.loads(r.text)["item"]["token"]["value"])
 except Exception as e:
-    print("Soething went wrong, the error is " + str(e))
+    print("Something went wrong, the error is " + str(e))
     quit()
 
 #parse the csv and upload it to JMS as json
 
 with open(input_file) as csvfile:
     reader = csv.DictReader(csvfile)
-    title = reader.fieldnames
     for line in reader:
         csv_lines = {}
         null = None
         csv_lines["customerId"] = null
-        for i in range(len(title)):
-            csv_lines[title[i]] = line[title[i]]
+        addresses = ["address", "city", "zipCode", "country", "isDefault"]
+        contacts = ["name", "value", "type"]
+        vehicles = ["plate", "color", "model", "year", "state"]
+        csv_lines = {k: v for k, v in line.items() if k not in addresses and k not in contacts and k not in vehicles}
+        csv_lines["addresses"] = [{k: v for k, v in line.items() if k in addresses}]
+        csv_lines["contacts"] = [{k: v for k, v in line.items() if k in contacts}]
+        csv_lines["vehicles"] = [{k: v for k, v in line.items() if k in vehicles}]
         create_url = "http://" + jms + ":8080/janus-integration/api/ext/customer/create"
         headers = { "Content-Type": "application/json" , "Accept": "application/json", "Janus-TP-Authorization": token }
         data = (json.dumps(csv_lines, sort_keys=False, indent=4, separators=(",",": "), ensure_ascii=False))
+        #print(data)
         try:
             r = requests.post(create_url, headers=headers, data=data, timeout=10.0)
             with open("create_customer_results.log", "a") as log:
                 log.write( now.strftime("%Y-%m-%d %H:%M:%S") + "  Customer inserted: " + r.text + ' ' + "Response Code from JMS was: " + str(r.status_code) + "\n" )
+                if r.status_code != 200:
+                    print("An error occurred creating the customer " + csv_lines["firstName"] + ' ' + csv_lines["lastName"] + ", please check the log file")
+                else:
+                    print("Created customer " + csv_lines["firstName"] + ' ' + csv_lines["lastName"])
         except Exception as e:
-            print("Soething went wrong, the error is " + str(e))
+            print("Something went wrong, the error is " + str(e))
 print("The upload is done, please check on JMS that the customers are there. The log file is upload_results.log.")
