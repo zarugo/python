1b21f2640f5da3babffe3de3716d213bbf68fc8e
Luigi Zarlenga <luigi.zarlenga@faacspa.local>
2020-01-02 09:42:32 +0100

    modulize login

--- actions.py
+++ actions.py
@@ -1,4 +1,4 @@
-import os, sys, csv, json, requests
+import login, os, sys, csv, json, requests
 
 
 jms = input("Type the ip of JMS:  ")
@@ -7,20 +7,7 @@ password = input("Type the password of the third party:  ")
 
 # Do the login and get the token every time
 
-auth_url = "http://" + jms + ":8080/janus-integration/api/ext/login"
-log_headers = { "Content-Type": "application/json" , "Accept": "application/json"}
-logindata = { "username": username,	"password": password }
-try:
-    r = requests.post(auth_url, json=logindata, headers=log_headers, timeout=10.0)
-    if r.status_code == 401:
-        print("The user or password are not correct. Verify that the Third Party account is set up on JMS")
-        exit()
-    else:
-        token = (json.loads(r.text)["item"]["token"]["value"])
-except Exception as e:
-    print("Something went wrong, the error is " + str(e))
-    quit()
-
+token = login.login(jms, username, password)
 
 
 class Device:
