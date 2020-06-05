a4c0f7bff471de3f6463575043e0d98d493ef9db
zarugo <zar@pop-os.localdomain>
2020-06-05 23:47:01 +0200

    added gui elements

--- update_xegu.py
+++ update_xegu.py
@@ -12,14 +12,14 @@ import os
 try:
     import paramiko
 except:
-    subprocess.call([sys.executable, "-m", "ensurepip" ])
-    subprocess.call([sys.executable, "pip3", "install", "paramiko-ng"])
+    subprocess.call([sys.executable, '-m', 'ensurepip' ])
+    subprocess.call([sys.executable, 'pip3', 'install', 'paramiko-ng'])
 finally:
     import paramiko
 try:
     import easygui
 except:
-    subprocess.call([sys.executable, "pip3", "install", "easygui"])
+    subprocess.call([sys.executable, 'pip3', 'install', 'easygui'])
 finally:
     import easygui as eg
 
@@ -44,44 +44,53 @@ class XeguDevice:
         client.load_system_host_keys()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
-            client.connect(ip, username="root", password="hubparking")
+            client.connect(ip, username='root', password='hubparking', timeout=5)
         except ConnectionRefusedError:
-            sys.exit(eg.msgbox(msg="The connection to the device has been refused, please check the IP address, user and password on the device (must be 'root' and 'hubparking'). The update has failed!", title='Error!', ok_button='OK',image=None,root=None))
+            sys.exit(eg.msgbox(msg='The connection to the device has been refused, please check the IP address. The update has failed!', title='Error!', ok_button='OK',image=None,root=None))
         except OSError:
-            sys.exit(eg.msgbox(msg="No route to this IP addres, please check that it's correct. The update has failed!", title='Error!', ok_button='OK',image=None,root=None))
+            sys.exit(eg.msgbox(msg='No route to this IP addres, please check that it\'s correct. The update has failed!', title='Error!', ok_button='OK',image=None,root=None))
         except TimeoutError:
-            sys.exit(eg.msgbox(msg="The connection has timed out, please check please check the IP address.", title='Error!', ok_button='OK',image=None,root=None))
+            sys.exit(eg.msgbox(msg='The connection has timed out, please check please check the IP address.', title='Error!', ok_button='OK',image=None,root=None))
         try:
-            stdin, stdout, stderr = client.exec_command('fbset | grep \"mode \" | awk \'{print $2; }\' | tr -d \"\\"\" | cut -d \"-\" -f1')
+            stdin, stdout, stderr = client.exec_command('fbset | grep \"mode \" | awk \'{print $2}\' | tr -d \"\\"\" | cut -d \"-\" -f1')
             size = str(stdout.read())
         except paramiko.ssh_exception.SSHException:
-            sys.exit("It's impossible to understand the type of the display (7, 10.1 or 15.6 inches), please check the ssh connection. The update has failed!")
-        if "800x480" in size:
-            self.hw = "small"
-        elif "1366x768" in size:
-            self.hw = "big"
-        elif "1280x800" in size:
-            self.hw = "big"
-        elif "1920x1080" in size:
-            self.hw = "big"
+            sys.exit(eg.msgbox(msg='It\'s impossible to understand the type of the display (7, 10.1 or 15.6 inches), please check the ssh connection. The update has failed!', title='Error!', ok_button='OK',image=None,root=None))
+        if '800x480' in size:
+            self.hw = '7 Inches'
+        elif '1366x768' in size:
+            self.hw = '15.6 Inches HDR'
+        elif '1280x800' in size:
+            self.hw = '10.1 Inches'
+        elif '1920x1080' in size:
+            self.hw = '15.6 Inches FHD'
         else:
-            raise Exception("It's impossible to understand the type of the display (7, 10.1 or 15.6 inches), please check the ssh connection. The update has failed!")
+            sys.exit(eg.msgbox(msg='It\'s impossible to understand the type of the display (7, 10.1 or 15.6 inches), please check the ssh connection. The update has failed!', title='Error!', ok_button='OK',image=None,root=None))
 
 #Get all the info we need on the device (hardware type and configuration)
-print("Checking the display size...")
 device = XeguDevice(ip)
 
-if device.hw == "small":
-    if os.path.exists("./Software/7_Inch/xegu"):
-        client = paramiko.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(ip, username="root", password="hubparking")
-        sftp = client.open_sftp()
-        sftp.put("./Software/7_Inch/xegu", "/home/root/xegu")
-        stdin, stdout, stderr = client.exec_command("chmod +x /home/root/xegu; reboot")
-    else:
-        sys.exit("The installation package for the 7 inches Xegu does not exist in this folder, the update has failed!")
+eg.msgbox(msg='The display is a ' + device.hw + ' model, please select the correct xegu software', title=device.hw, ok_button='Select file',image=None,root=None)
 
+binary = eg.fileopenbox(msg=None, title='xegu software for ' + device.hw, default=None, filetypes=None, multiple=False)
 
-print(device.hw)
+client = paramiko.SSHClient()
+client.load_system_host_keys()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(ip, username='root', password='hubparking')
+sftp = client.open_sftp()
+try:
+    sftp.put(binary, '/home/root/xegu.new')
+except:
+    sys.exit(eg.msgbox(msg='The xegu file upload failed with the error ' + Exception + '. The update has failed.'))
+
+stdin, stdout, stderr = client.exec_command('mv /home/root/xegu /home/root/xegu.old ; mv /home/root/xegu.new /home/root/xegu ; chmod +x /home/root/xegu')
+
+eg.msgbox(msg='Please select the language file', title=device.hw, ok_button='Select file',image=None,root=None)
+
+language = eg.fileopenbox(msg=None, title='Language file selection', default=None, filetypes='*.xml', multiple=False)
+
+try:
+    sftp.put(language, '/home/root/.local/xegulang.xml')
+except:
+    sys.exit(eg.msgbox(msg='The language file upload failed with the error ' + Exception + '. The update has failed.'))
