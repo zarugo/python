bc96c4c9d24b80364bff39fff84a1d754f65084e
Luigi Zarlenga <luigi.zarlenga@faacspa.local>
2020-06-08 10:10:21 +0200

    more

--- update_xegu.py
+++ update_xegu.py
@@ -13,13 +13,13 @@ try:
     import paramiko
 except:
     subprocess.call([sys.executable, '-m', 'ensurepip' ])
-    subprocess.call([sys.executable, 'pip3', 'install', 'paramiko-ng'])
+    subprocess.call([sys.executable, '-m', 'pip', 'install', 'paramiko-ng'])
 finally:
     import paramiko
 try:
     import easygui
 except:
-    subprocess.call([sys.executable, 'pip3', 'install', 'easygui'])
+    subprocess.call([sys.executable, '-m', 'pip', 'install', 'easygui'])
 finally:
     import easygui as eg
 
@@ -74,23 +74,61 @@ eg.msgbox(msg='The display is a ' + device.hw + ' model, please select the corre
 
 binary = eg.fileopenbox(msg=None, title='xegu software for ' + device.hw, default=None, filetypes=None, multiple=False)
 
+if device.hw in '7 Inches':
+    with open("xegu.sh", "w", newline='\n') as script:
+        script.write('''HOME=/home/root
+export QMLSCENE_DEVICE=softwarecontext
+
+while [ 1 ]
+do
+        echo \# Launch \"xegu\" app...
+        date
+
+        cd /home/root
+        ./xegu -platform linuxfb > /dev/null 2>&1
+        sleep 1
+done
+# exit 0
+''')
+
 client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(ip, username='root', password='hubparking')
 sftp = client.open_sftp()
-try:
-    sftp.put(binary, '/home/root/xegu.new')
-except:
-    sys.exit(eg.msgbox(msg='The xegu file upload failed with the error ' + Exception + '. The update has failed.'))
+if device.hw not in '7 Inches':
+    try:
+        sftp.put(binary, '/home/root/xegu.new')
+    except:
+        sys.exit(eg.msgbox(msg='Unable to upload the xegu file, the update has failed.'))
+else:
+    try:
+        stdin, stdout, stderr = client.exec_command('find /home/root/ -regex ".*xegu.*old" -delete')
+        stdin, stdout, stderr = client.exec_command('df -h | grep ubi0 | awk \'{print $5}\' | cut -d \'%\' -f1')
+        free = 100 - float(stdout.read())
+        if free < 14:
+            sys.exit(eg.msgbox(msg='The device has only ' + str(free) + '% of free space, we need at least 14%. Please check and free enough space, the update has failed.'))
+        else:
+            sftp.put(binary, '/home/root/xegu.new')
+            sftp.put('./xegu.sh', 'home/root/xegu.sh')
+    except:
+        sys.exit(eg.msgbox(msg='Unable to upload the xegu file, the update has failed.'))
 
-stdin, stdout, stderr = client.exec_command('mv /home/root/xegu /home/root/xegu.old ; mv /home/root/xegu.new /home/root/xegu ; chmod +x /home/root/xegu')
+stdin, stdout, stderr = client.exec_command('mv /home/root/xegu /home/root/xegu.old ; mv /home/root/xegu.new /home/root/xegu ; chmod +x /home/root/xegu /home/root/xegu.sh')
 
 eg.msgbox(msg='Please select the language file', title=device.hw, ok_button='Select file',image=None,root=None)
 
 language = eg.fileopenbox(msg=None, title='Language file selection', default=None, filetypes='*.xml', multiple=False)
 
 try:
-    sftp.put(language, '/home/root/.local/xegulang.xml')
+    sftp.put(language, '/home/root/.local/share/xegu/xegulang.xml')
 except:
     sys.exit(eg.msgbox(msg='The language file upload failed with the error ' + Exception + '. The update has failed.'))
+
+if eg.ccbox(msg='The xegu and xegulang.xml files have been updloaded, please reboot the display to use the new release', title=device.hw, choices=('Reboot', 'Cancel')):
+    pass
+else:
+    sys.exit(eg.msgbox(msg='Please remember to reboot the display manually later.'))
+stdin, stdout, stderr = client.exec_command('reboot')
+os.remove('xegu.sh')
+eg.msgbox(msg='The update is finished')
