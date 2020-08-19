cbeeb64489a04f8bbc937922c1a352a0e67b2c34
zarugo <zar@pop-os.localdomain>
2020-08-19 19:39:52 +0200

    we do not need this on github

--- JPSApps_jhw/JPSApplication/StartWiFiAP.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -x
-
-/home/root/pal_utils/control_ap_wf111 start wlan0 eth0
--- JPSApps_jhw/JPSDemoLua_TestCase/run.sh
+++
@@ -1,4 +0,0 @@
-export LD_LIBRARY_PATH=../libs/
-chmod +x JPSDemoLua_TestCase*
-./JPSDemoLua_TestCase*
-#valgrind --leak-check=yes --num-callers=10 --time-stamp=yes --track-origins=yes --log-file=./vlgrnd.log ./JPSDemoLua_TestCase*
